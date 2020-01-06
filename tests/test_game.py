7289b916d6a45c0106e8cfb154cac50b69ef728c
RAVEN DELANEY <ravend17@gmail.com>
2020-01-06 13:34:33 -0800

    added import to test files

---
+++ tests/test_game.py
@@ -0,0 +1,11 @@
+# https://stackoverflow.com/questions/10253826/path-issue-with-pytest-importerror-no-module-named-yadayadayada
+import sys, os
+myPath = os.path.dirname(os.path.abspath(__file__))
+sys.path.insert(0, myPath + '/../scripts/')
+
+
+from game import Game
+
+
+def test_game_instance():
+  assert Game()
---
+++ tests/test_hand.py
@@ -0,0 +1,8 @@
+# https://stackoverflow.com/questions/10253826/path-issue-with-pytest-importerror-no-module-named-yadayadayada
+import sys, os
+myPath = os.path.dirname(os.path.abspath(__file__))
+sys.path.insert(0, myPath + '/../scripts/player/')
+
+
+from hand import Hand
+
