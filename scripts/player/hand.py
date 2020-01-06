3fc061cb320dcedafc0f780c8ed44521209d9b58
Chris Stanley <CStanley4848@gmail.com>
2020-01-06 13:08:07 -0800

    Updated the Pipfile to version 3

--- scripts/player/hand.py
+++ scripts/player/hand.py
@@ -1,7 +1,11 @@
 class Hand:
   def __init__(self):
-    pass
+    self.cards = []
 
   # Calcualte Score
+  def socre(self):
+    pass
 
   # Keep Current Hand
+  def add_card(self, card):
+    self.cards.append(card)
