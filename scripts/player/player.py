5fbb1dbc2799812522ff29593c266576d12b7836
Joseph Hangarter <joseph.hangarter@yahoo.com>
2020-01-06 15:07:22 -0800

    Moved methods from hand.py to player.py, and removed hand.py

--- scripts/player/player.py
+++ scripts/player/player.py
@@ -2,14 +2,48 @@ from hand import Hand
 
 class Player:
   def __init__(self):
-    self.hand = Hand()
+    self.hand = []
+
 
   def hit(self, card):
     # Add new card to their hand
-    self.hand.add_card(card)
+    """
+    Takes in a card and adds it to the players hand
+    In: Card instance
+    Exceptions: None
+    Out: None
+    """
+    self.cards.append(card)
+
 
   def get_score(self):
-    return self.hand.score()
+    """
+    In: None
+    Exceptions: Empty Hand Raises Error
+    Out: Current Score of the players hand
+    """
+    if len(self.cards) == 0:
+      raise EmptyHandError
+
+    score = 0
+    aces = 0
+    for card in self.cards:
+      if card.name == 'Ace':
+        aces += 1
+      else:
+        score += card.value
+
+    for ace in range(0, aces):
+      if score + 11 <= 21:
+        if score + 11 <= 21 and ace == aces - 1:
+          score += 11
+        else:
+          score += 1
+      else:
+        score += 1
+
+    return score
+
 
   def bust(self):
     return self.get_score() > 21
@@ -18,25 +52,38 @@ class Dealer(Player):
   def __init__(self):
     super(Dealer, self).__init__()
 
+
 class User(Player):
   def __init__(self):
     super(User, self).__init__()
     self.bank = 100
     self.bet = 0
 
+
   def place_bet(self, bet):
     if not isinstance(bet, int):
       raise ValueError
     self.bet = bet
 
+
   def get_bank(self):
     return self.bank
 
+
   def get_bet(self):
     return self.bet
+
 
   def beat_dealer(self, win = False):
     if win:
       self.bank += self.bet
     else:
       self.bank -= self.bet
+      
+
+###################
+## Unique Errors ##
+###################
+
+class EmptyHandError(ValueError):
+  pass
