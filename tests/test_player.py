ac66a9566bec0ae1559416686e641ce9bee66113
RAVEN DELANEY <ravend17@gmail.com>
2020-01-06 16:57:47 -0800

    parametrized tests

--- tests/test_player.py
+++ tests/test_player.py
@@ -3,7 +3,7 @@ import sys, os
 myPath = os.path.dirname(os.path.abspath(__file__))
 sys.path.insert(0, myPath + '/../scripts/player/')
 sys.path.insert(1, myPath + '/../scripts/deck/')
-
+import pytest
 
 from player import Player, User
 from card import Card
@@ -14,25 +14,41 @@ def test_import():
 def test_player_instance():
   assert Player()
 
-def test_player_score_21():
+
+@pytest.mark.parametrize("test_input_1, test_input_2, expected",
+[(Card(10), Card(1), 21),
+(Card(1), Card(1), 12),
+(Card(1), Card(6), 17)])
+def test_score_2_cards(test_input_1, test_input_2, expected):
   user = User()
-  user.hit(Card(10))
-  user.hit(Card(1))
-  assert user.get_score() == 21
+  user.hit(test_input_1)
+  user.hit(test_input_2)
+  assert user.get_score() == expected
 
-def test_2_aces():
+
+@pytest.mark.parametrize("test_input_1, test_input_2, test_input_3, expected",
+[(Card(10), Card(8), Card(9), 27),
+(Card(6), Card(5), Card(9), 20),
+(Card(1), Card(1), Card(5), 17)])
+def test_score_3_cards(test_input_1, test_input_2, test_input_3, expected):
   user = User()
-  user.hit(Card(1))
-  user.hit(Card(1))
-  assert user.get_score() == 12
+  user.hit(test_input_1)
+  user.hit(test_input_2)
+  user.hit(test_input_3)
+  assert user.get_score() == expected
 
-def test_17():
+
+@pytest.mark.parametrize("test_input_1, test_input_2, test_input_3, test_input_4, expected",
+[(Card(10), Card(3), Card(5), Card(4), 22),
+(Card(6), Card(5), Card(9), Card(1), 21),
+(Card(1), Card(1), Card(5), Card(6), 13)])
+def test_score_4_cards(test_input_1, test_input_2, test_input_3, test_input_4, expected):
   user = User()
-  user.hit(Card(1))
-  user.hit(Card(6))
-  assert user.get_score() == 17
-
-
+  user.hit(test_input_1)
+  user.hit(test_input_2)
+  user.hit(test_input_3)
+  user.hit(test_input_4)
+  assert user.get_score() == expected
 
 
 
