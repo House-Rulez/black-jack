bada6984f1e3a8a9232854712176ba319a80a354
RAVEN DELANEY <ravend17@gmail.com>
2020-01-06 16:11:14 -0800

    more passing tests

--- tests/test_player.py
+++ tests/test_player.py
@@ -2,12 +2,37 @@
 import sys, os
 myPath = os.path.dirname(os.path.abspath(__file__))
 sys.path.insert(0, myPath + '/../scripts/player/')
+sys.path.insert(1, myPath + '/../scripts/deck/')
 
 
-from player import Player
+from player import Player, User
+from card import Card
 
 def test_import():
   assert Player
 
 def test_player_instance():
   assert Player()
+
+def test_player_score_21():
+  user = User()
+  user.hit(Card(10))
+  user.hit(Card(1))
+  assert user.get_score() == 21
+
+def test_2_aces():
+  user = User()
+  user.hit(Card(1))
+  user.hit(Card(1))
+  assert user.get_score() == 12
+
+def test_17():
+  user = User()
+  user.hit(Card(1))
+  user.hit(Card(6))
+  assert user.get_score() == 17
+
+
+
+
+
